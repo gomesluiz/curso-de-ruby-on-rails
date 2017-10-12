a6a7d628984d3f2d8669598592c274aadcaf0b22
Luiz Alberto <gomes.luiz@gmail.com>
2017-10-12 02:09:11 -0300

    atualizacao de conteudo

---
+++ codigos/ruby/02-fluxo-de-controle/if.rb
@@ -0,0 +1,9 @@
+a = 5
+if  a == 3                    
+  puts "a igual a 3"                  
+elsif a == 5 
+  puts "a igual a 5"                   
+else                              
+  puts "a nao e igual 3 ou 5"
+end 
+# => a e igual a 5
