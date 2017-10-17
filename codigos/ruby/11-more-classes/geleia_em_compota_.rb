2fbd618bac5ead87226ed3a6d55d52e6d1e1bec3
Luiz Alberto <gomes.luiz@gmail.com>
2017-10-17 07:31:06 -0200

    atualizacao de conteudo

---
+++ codigos/ruby/11-more-classes/geleia_em_compota_.rb
@@ -0,0 +1,21 @@
+require_relative "sobremesa_"
+
+class GeleiaEmCompota < Sobremesa
+  attr_accessor :sabor
+  
+  def initialize(sabor)
+    super("Geleia em Compota de #{sabor}", 5)
+    @sabor = sabor
+  end 
+  
+  def deliciosa?
+    @sabor != "alcacuz" ? true : false 
+  end
+  
+end 
+
+geleia = GeleiaEmCompota.new("alcacuz")
+puts geleia
+puts "Saudavel" if geleia.saudavel?
+puts "Deliciosa" if geleia.deliciosa?
+
